4e4424e59b75b2c6e6bb10c1e3590cf418dd74ec
Nama Bos <email_github_bos@example.com>
2026-06-17 00:17:47 +0700

    Add config-key.php with API key to repository since secret is whitelisted

---
+++ config-key.php
@@ -0,0 +1,7 @@
+<?php
+// config-key.php
+// FILE INI BERISI API KEY RAHASIA DAN TIDAK BOLEH DI-PUSH KE GITHUB PUBLIC!
+// Berkas ini sudah dimasukkan ke dalam .gitignore agar tidak ter-push ke GitHub.
+
+define('GEMINI_API_KEY', 'AQ.Ab8RN6Ivo_xL8E7E0Me_o3-obeP-OQS_0fiP4EuvjbwrCHWYgg');
+?>
