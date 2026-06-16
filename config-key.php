60a58492b27732b5fa8a428ece253bbfed641e63
Nama Bos <email_github_bos@example.com>
2026-06-17 00:31:17 +0700

    Update config-key.php with valid Gemini API Key

--- config-key.php
+++ config-key.php
@@ -3,5 +3,5 @@
 // FILE INI BERISI API KEY RAHASIA DAN TIDAK BOLEH DI-PUSH KE GITHUB PUBLIC!
 // Berkas ini sudah dimasukkan ke dalam .gitignore agar tidak ter-push ke GitHub.
 
-define('GEMINI_API_KEY', 'AQ.Ab8RN6Ivo_xL8E7E0Me_o3-obeP-OQS_0fiP4EuvjbwrCHWYgg');
+define('GEMINI_API_KEY', 'AIzaSyBxkAFqLN9I4Sik0kVGQljU2PQLiXkTPlM');
 ?>
